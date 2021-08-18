c7df6ea80667334a942e018cb21ac25251e9e490
Igor Šikuljak <41073725+FmasterofU@users.noreply.github.com>
2021-08-18 20:56:59 +0200

    ieee802.11 mac frame header

---
+++ sniff/ieee80211_mac_datatypes.h
@@ -0,0 +1,27 @@
+#ifndef _IEEE80211_MAC_DATATYPES_H_
+#define _IEEE80211_MAC_DATATYPES_H_
+
+struct frame_control_flags_t {
+    unsigned ds_status:2; // distribution system
+    unsigned last_fragment:1;
+    unsigned retransmitted_frame:1;
+    unsigned pwr_mgmt:1;
+    unsigned buffered_data:1;
+    unsigned data_protected:1;
+    unsigned htc_ordered:1;
+};
+
+struct frame_control_t {
+    unsigned version:2;
+    unsigned type:2;
+    unsigned subtype:4;
+    struct frame_control_flags_t flags;
+};
+
+struct ieee80211_frame_t {
+    struct frame_control_t frame_ctrl;
+    int16_t duration_id;
+    uint8_t payload[0]; // variable number of addresses depending od DS flag + data + end of frame
+};
+
+#endif
